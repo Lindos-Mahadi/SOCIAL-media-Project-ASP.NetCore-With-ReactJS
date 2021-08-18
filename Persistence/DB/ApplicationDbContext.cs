28a56bc5c745020f6e780fe5e2fdb823d15ec6ec
Lindos-Mahadi <lindosmahadi@.com>
2021-08-18 10:37:05 +0600

    Initial SM Project

---
+++ Persistence/DB/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DomainModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Value> Values { get; set; }
+    }
+}
