af5f9f007938463cf140dac5a3886c4154766dca
Lindos-Mahadi <lindosmahadi@.com>
2021-08-18 11:15:50 +0600

    Seeding data using Entity Framework fluent configuration

--- Persistence/DB/ApplicationDbContext.cs
+++ Persistence/DB/ApplicationDbContext.cs
@@ -16,5 +16,18 @@ namespace Persistence.DB
         }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Value 1"},
+                    new Value { Id = 2, Name = "Value 2" },
+                    new Value { Id = 3, Name = "Value 3" },
+                    new Value { Id = 4, Name = "Value 4" },
+                    new Value { Id = 5, Name = "Value 5" }
+                );
+        }
+
     }
 }
--- Persistence/Migrations/20210818051225_SeedValue.Designer.cs
+++ Persistence/Migrations/20210818051225_SeedValue.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.DB;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210818051225_SeedValue")]
+    partial class SeedValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
